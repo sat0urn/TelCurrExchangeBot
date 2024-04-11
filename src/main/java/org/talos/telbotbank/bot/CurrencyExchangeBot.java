b4aba54831931d480bc1b606332c6bfd7c1e83eb
Aslan <nissanfordo03@gmail.com>
2024-04-11 22:57:43 +0500

    new

--- src/main/java/org/talos/telbotbank/bot/CurrencyExchangeBot.java
+++ src/main/java/org/talos/telbotbank/bot/CurrencyExchangeBot.java
@@ -15,8 +15,8 @@ import java.text.ParseException;
 @Component
 public class CurrencyExchangeBot extends TelegramLongPollingBot {
 
-    public CurrencyExchangeBot(@Value("${TG_TOKEN}") String botToken) {
-        super(System.getenv("TG_TOKEN"));
+    public CurrencyExchangeBot(@Value("${bot.token}") String botToken) {
+        super(botToken);
     }
 
     @Override
